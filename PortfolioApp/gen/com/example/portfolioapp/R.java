14857ee5453c12badcd8effc175743abba9a3963
Jimmi Elofsson <jimmielofsson@gmail.com>
2013-10-11 14:40:14 +0200

    Checkar in R.java

--- PortfolioApp/gen/com/example/portfolioapp/R.java
+++ PortfolioApp/gen/com/example/portfolioapp/R.java
@@ -25,18 +25,24 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_next=0x7f080005;
-        public static final int action_previous=0x7f080004;
-        public static final int action_settings=0x7f080003;
+        public static final int action_next=0x7f080009;
+        public static final int action_previous=0x7f080008;
+        public static final int action_settings=0x7f080007;
         public static final int content=0x7f080002;
         public static final int gotoMainBtn=0x7f080001;
+        public static final int mediaPager=0x7f080006;
         public static final int pager=0x7f080000;
+        public static final int picture=0x7f080003;
+        public static final int text=0x7f080005;
+        public static final int video=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_project=0x7f030001;
         public static final int activity_splash=0x7f030002;
-        public static final int fragment_screen_slide_page=0x7f030003;
+        public static final int fragment_screen_slide_media=0x7f030003;
+        public static final int fragment_screen_slide_page=0x7f030004;
+        public static final int fragment_screen_slide_project=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
